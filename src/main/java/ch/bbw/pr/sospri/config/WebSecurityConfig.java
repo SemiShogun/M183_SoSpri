3ce1ed893a6539c7d1ffded959a872e97a3754aa
SemiShogun <nectoj@gmail.com>
2021-05-25 14:23:29 +0200

    Add 403 and 404 error pages

--- src/main/java/ch/bbw/pr/sospri/config/WebSecurityConfig.java
+++ src/main/java/ch/bbw/pr/sospri/config/WebSecurityConfig.java
@@ -1,30 +1,60 @@
 package ch.bbw.pr.sospri.config;
 
+import ch.bbw.pr.sospri.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("password").roles("USER","ADMIN");
+    MemberService memberservice;
+
+//    @Autowired
+//    public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("{noop}1234").roles("user").and()
+//                .withUser("admin").password("{noop}5678").roles("user", "admin");
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(this.memberservice);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/get-channel").permitAll()
+        http.
+                csrf().ignoringAntMatchers("/h2-console/**").disable()
+                .authorizeRequests()
+                .antMatchers("/css/*", "/fragments/*", "/img/*", "/errors/*").permitAll()
+                .antMatchers("/get-channel").hasAnyAuthority("admin","supervisor","member")
+                .antMatchers("/get-members").hasAuthority("admin")
                 .anyRequest().authenticated()
-                .and().formLogin().permitAll();
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll()
+                .and().exceptionHandling().accessDeniedPage("/errors/403.html");
     }
-
-    @Override
-    protected void conffi
 
 }
