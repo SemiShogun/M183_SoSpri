dfe955d386699566e75ebd15edc5a4bfcd1a6184
SemiShogun <nectoj@gmail.com>
2021-07-14 14:51:19 +0200

    Add Recaptcha

--- src/main/java/ch/bbw/pr/sospri/Controller/RegisterController.java
+++ src/main/java/ch/bbw/pr/sospri/Controller/RegisterController.java
@@ -1,6 +1,9 @@
 package ch.bbw.pr.sospri.Controller;
 
+import ch.bbw.pr.sospri.captcha.ReCaptchaValidationService;
 import ch.bbw.pr.sospri.member.Member;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import ch.bbw.pr.sospri.member.MemberService;
 import ch.bbw.pr.sospri.member.RegisterMember;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -26,7 +30,12 @@ import javax.validation.Valid;
 @Controller
 public class RegisterController {
     @Autowired
+    private ReCaptchaValidationService validator;
+
+    @Autowired
     MemberService memberservice;
+
+    private static final Logger logger = LogManager.getLogger(MembersController.class);
 
     private static final String PEPPER = "DingDongChingChong";
 
@@ -41,40 +50,47 @@ public class RegisterController {
     }
 
     @PostMapping("/get-register")
-    public String postRequestRegistMembers(@Valid RegisterMember registerMember, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
+    public String postRequestRegistMembers(@Valid RegisterMember registerMember, BindingResult bindingResult, @RequestParam(name="g-recaptcha-response") String captcha, Model model) {
+
+        if (validator.validateCaptcha(captcha)) {
+            if (bindingResult.hasErrors()) {
 //			model.addAttribute("registerMember", new RegisterMember());
-            return "register";
+                return "register";
+            }
+
+            if (!registerMember.getPassword().equals(registerMember.getConfirmation())) {
+                model.addAttribute("registerMember", new RegisterMember());
+                model.addAttribute("message", "Password and Password confirmation are not equal!");
+                return "register";
+            }
+
+            if (memberservice.getByUserName(registerMember.getPrename().toLowerCase() + "." +
+                    registerMember.getLastname().toLowerCase()) != null) {
+                model.addAttribute("message", "Username " + registerMember.getPrename().toLowerCase() + "." +
+                        registerMember.getLastname().toLowerCase() + " already exists");
+
+                return "register";
+            }
+
+            Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder(PEPPER, 185000, 256);
+            String encodedpw = encoder.encode(registerMember.getPassword());
+
+            Member member = new Member();
+            member.setAuthority("member");
+            member.setPrename(registerMember.getPrename());
+            member.setLastname(registerMember.getLastname());
+            member.setPassword(encodedpw);
+            member.setUsername(member.getPrename() + "." + member.getLastname());
+
+            memberservice.add(member);
+
+            model.addAttribute("username", member.getUsername());
+            return "registerconfirmed";
+
+        } else {
+            model.addAttribute("message", "Please verify Captcha");
+            logger.error("invalid captcha");
         }
-
-        if (!registerMember.getPassword().equals(registerMember.getConfirmation())) {
-            model.addAttribute("registerMember", new RegisterMember());
-            model.addAttribute("message", "Password and Password confirmation are not equal!");
-            return "register";
-        }
-
-        if (memberservice.getByUserName(registerMember.getPrename().toLowerCase() + "." +
-                registerMember.getLastname().toLowerCase()) != null) {
-            model.addAttribute("message", "Username " + registerMember.getPrename().toLowerCase() + "." +
-                    registerMember.getLastname().toLowerCase() + " already exists");
-
-            return "register";
-        }
-
-        Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder(PEPPER, 185000, 256);
-        String encodedpw = encoder.encode(registerMember.getPassword());
-
-        Member member = new Member();
-        member.setAuthority("member");
-        member.setPrename(registerMember.getPrename());
-        member.setLastname(registerMember.getLastname());
-        member.setPassword(encodedpw);
-        member.setUsername(member.getPrename() + "." + member.getLastname());
-
-        memberservice.add(member);
-
-        model.addAttribute("username", member.getUsername());
-
-        return "registerconfirmed";
+        return "register";
     }
 }
